9e0abfe484dd0aa4df070b8047f913390cdbd831
Rhama Krisner <rhamakrisner@gmail.com>
2023-07-05 18:44:33 -0300

    Adicionado MaterialFX

--- src/main/java/app/cadastrodealunos/HelloController.java
+++ src/main/java/app/cadastrodealunos/HelloController.java
@@ -1,11 +1,13 @@
 package app.cadastrodealunos;
 
 import javafx.fxml.FXML;
+import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 
 public class HelloController {
     @FXML
     private Label welcomeText;
+
 
     @FXML
     protected void onHelloButtonClick() {
